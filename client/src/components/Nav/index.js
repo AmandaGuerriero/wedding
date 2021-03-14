45b56b726c98f782c15934ce52845aaec1cd38d4
Amanda Guerriero <ag.codingclass@gmail.com>
2021-03-14 16:32:15 -0700

    working on logos in header

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -5,33 +5,39 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function Nav() {
   return (
-    <div className="navbar">
-      <FontAwesomeIcon icon={faHome} />
-      <div className="navbar-right">
-        <a href="#">OUR STORY</a>
-        <div className="dropdown">
-          <button className="dropbtn">LOGISTICS	&nbsp;
-          <i className="fa fa-caret-down"></i>
-          </button>
-          <div className="dropdown-content">
-            <a href="#">TRAVEL</a>
-            <a href="#">LODGING</a>
-            <a href="#">ADVENTURE</a>
-          </div>
-        </div>
-        <div className="dropdown">
-          <button className="dropbtn">WEDDING &nbsp;
+    <div>
+      <div className="navbar">
+        <FontAwesomeIcon icon={faHome} />
+        <div className="navbar-right">
+          <a href="#">OUR STORY</a>
+          <div className="dropdown">
+            <button className="dropbtn">LOGISTICS	&nbsp;
             <i className="fa fa-caret-down"></i>
-          </button>
-          <div className="dropdown-content">
-            <a href="#">DETAILS</a>
-            <a href="#">SCHEDULE</a>
+            </button>
+            <div className="dropdown-content">
+              <a href="#">TRAVEL</a>
+              <a href="#">LODGING</a>
+              <a href="#">ADVENTURE</a>
+            </div>
           </div>
-        </div> 
-        <a href="/registry">REGISTRY</a>
-        <a href="/rsvp">RSVP</a>
+          <div className="dropdown">
+            <button className="dropbtn">WEDDING &nbsp;
+              <i className="fa fa-caret-down"></i>
+            </button>
+            <div className="dropdown-content">
+              <a href="#">DETAILS</a>
+              <a href="#">SCHEDULE</a>
+            </div>
+          </div> 
+          <a href="/registry">REGISTRY</a>
+          <a href="/rsvp">RSVP</a>
+        </div>
       </div>
-    </div> 
+      <div>
+      <img id="logo" src="../images/LOGO-01.png"/>
+      <img id="background" src="../images/LOGO-01.png"/>
+      </div>
+    </div>
   );
 }
 
