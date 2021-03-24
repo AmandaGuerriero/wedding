a29609ee5009b373d27f88432496663b5ebaad24
Amanda Guerriero <ag.codingclass@gmail.com>
2021-03-23 20:54:13 -0700

    Cloudinary Photos

--- client/src/components/InternationalPhotos/index.js
+++ client/src/components/InternationalPhotos/index.js
@@ -5,37 +5,37 @@ function InternationalPhotos() {
   return(
     <div className="row">
       <div className="column">
-        <img src='../images/international/20180203_145736.jpg' /><p>Jungfraujoch, Switzerland</p>
-        <img src='../images/international/IMG_2594.JPG' /><p>Plaza de las Americas, Sevilla</p>
-        <img src='../images/international/IMG_1731.JPG' /><p>Lake Thun, Switzerland</p>
-        <img src='../images/international/LISBON_4.jpg' /><p>Lisbon, Portugal</p>
-        <img src='../images/international/20180812_212533.jpg' /><p>Sevilla, Spain</p>
-        <img src='../images/international/20180818_175400.jpg' /><p>Porto, Portugal</p>
-        <img src='../images/international/20180816_200909.jpg' /><p>Lisbon, Portugal</p>
-        <img src='../images/international/20180814_210059.jpg' /><p>Sevilla, Spain</p>
-        <img src='../images/international/FB_IMG_1543580549458.jpg' /><p>Palacio de Pena, Portugal</p>
+        <img src='https://res.cloudinary.com/amandaeric/image/upload/international/20180203_145736.jpg' /><p>Jungfraujoch, Switzerland</p>
+        <img src='https://res.cloudinary.com/amandaeric/image/upload/international/IMG_2594.JPG' /><p>Plaza de las Americas, Sevilla</p>
+        <img src='https://res.cloudinary.com/amandaeric/image/upload/international/IMG_1731.JPG' /><p>Lake Thun, Switzerland</p>
+        <img src='https://res.cloudinary.com/amandaeric/image/upload/international/LISBON_4.jpg' /><p>Lisbon, Portugal</p>
+        <img src='https://res.cloudinary.com/amandaeric/image/upload/international/20180812_212533.jpg' /><p>Sevilla, Spain</p>
+        <img src='https://res.cloudinary.com/amandaeric/image/upload/international/20180818_175400.jpg' /><p>Porto, Portugal</p>
+        <img src='https://res.cloudinary.com/amandaeric/image/upload/international/20180816_200909.jpg' /><p>Lisbon, Portugal</p>
+        <img src='https://res.cloudinary.com/amandaeric/image/upload/international/20180814_210059.jpg' /><p>Sevilla, Spain</p>
+        <img src='https://res.cloudinary.com/amandaeric/image/upload/international/FB_IMG_1543580549458.jpg' /><p>Palacio de Pena, Portugal</p>
       </div>
       <div className="column">
-        <img src='../images/international/20180203_121410.jpg' /><p>Gridelwald, Switzerland</p>
-        <img src='../images/international/20180814_114542.jpg' /><p>Plaza de Espana, Sevilla</p>
-        <img src='../images/international/20180819_094752.jpg' /><p>Pre Port Tasting in Porto</p>
-        <img src='../images/international/20190317_185445.jpg' /><p>Turks & Caicos</p>
-        <img src='../images/international/IMG_2510.JPG' /><p>Lagos, Portugal</p>
-        <img src='../images/international/20190317_105253.jpg' /><p>Iguana Island</p>
-        <img src='../images/international/FB_IMG_1543580558450.jpg' /><p>Cabo de Roca, Portugal</p>
-        <img src='../images/international/54799851_10102040867187729_2263689404147040256_o.jpg' /><p>High Above Turks & Caicos</p>
-        <img src='../images/international/20190315_120837.JPG' /><p>Stuck Together</p>
+        <img src='https://res.cloudinary.com/amandaeric/image/upload/international/20180203_121410.jpg' /><p>Gridelwald, Switzerland</p>
+        <img src='https://res.cloudinary.com/amandaeric/image/upload/international/20180814_114542.jpg' /><p>Plaza de Espana, Sevilla</p>
+        <img src='https://res.cloudinary.com/amandaeric/image/upload/international/20180819_094752.jpg' /><p>Pre Port Tasting in Porto</p>
+        <img src='https://res.cloudinary.com/amandaeric/image/upload/international/20190317_185445.jpg' /><p>Turks & Caicos</p>
+        <img src='https://res.cloudinary.com/amandaeric/image/upload/international/IMG_2510.JPG' /><p>Lagos, Portugal</p>
+        <img src='https://res.cloudinary.com/amandaeric/image/upload/international/20190317_105253.jpg' /><p>Iguana Island</p>
+        <img src='https://res.cloudinary.com/amandaeric/image/upload/international/FB_IMG_1543580558450.jpg' /><p>Cabo de Roca, Portugal</p>
+        <img src='https://res.cloudinary.com/amandaeric/image/upload/international/54799851_10102040867187729_2263689404147040256_o.jpg' /><p>High Above Turks & Caicos</p>
+        <img src='https://res.cloudinary.com/amandaeric/image/upload/international/20190315_120837.JPG' /><p>Stuck Together</p>
       </div>
       <div className="column">
-        <img src='../images/international/20170207_051231.JPG' /><p>Lucerene, Switzerland</p>
-        <img src='../images/international/20180817_143439.jpg' /><p>Cabo de Roca, Portugal</p>
-        <img src='../images/international/20190713_132225.jpg' /><p>Montreal, Canada</p>
-        <img src='../images/international/IMG_1854.JPG' /><p>Switzerland</p>
-        <img src='../images/international/IMG_20190315_214137_561.jpg' /><p>Turks & Caicos</p>
-        <img src='../images/international/FB_IMG_1543580535094.jpg' /><p>Post-Fondue in Zurich</p>
-        <img src='../images/international/FB_IMG_1543580579136.jpg' /><p>Plaza de Espana in Sevilla</p>
-        <img src='../images/international/20170207_045803_01.JPG' /><p>Lucerene, Switzerland</p>
-        <img src='../images/international/20180815_192633.jpg' /><p>Lagos, Portugal</p>
+        <img src='https://res.cloudinary.com/amandaeric/image/upload/international/20170207_051231.JPG' /><p>Lucerene, Switzerland</p>
+        <img src='https://res.cloudinary.com/amandaeric/image/upload/international/20180817_143439.jpg' /><p>Cabo de Roca, Portugal</p>
+        <img src='https://res.cloudinary.com/amandaeric/image/upload/international/20190713_132225.jpg' /><p>Montreal, Canada</p>
+        <img src='https://res.cloudinary.com/amandaeric/image/upload/international/IMG_1854.JPG' /><p>Switzerland</p>
+        <img src='https://res.cloudinary.com/amandaeric/image/upload/international/IMG_20190315_214137_561.jpg' /><p>Turks & Caicos</p>
+        <img src='https://res.cloudinary.com/amandaeric/image/upload/international/FB_IMG_1543580535094.jpg' /><p>Post-Fondue in Zurich</p>
+        <img src='https://res.cloudinary.com/amandaeric/image/upload/international/FB_IMG_1543580579136.jpg' /><p>Plaza de Espana in Sevilla</p>
+        <img src='https://res.cloudinary.com/amandaeric/image/upload/international/20170207_045803_01.JPG' /><p>Lucerene, Switzerland</p>
+        <img src='https://res.cloudinary.com/amandaeric/image/upload/international/20180815_192633.jpg' /><p>Lagos, Portugal</p>
       </div>
     </div>  
   )
