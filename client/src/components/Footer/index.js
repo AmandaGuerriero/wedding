a236e5527645e27bdf5e99e7cd1f0e4b6ba53569
Amanda Guerriero <ag.codingclass@gmail.com>
2021-04-28 22:26:52 -0700

    Fixed footer

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -5,7 +5,7 @@ import './style.css'
 function Footer() {
     return (
         <footer className= "flex-row space-between px-1">
-           <p> ♥ We created this website from scratch together with love. Developed by Amanda & Designed by Eric ♥ </p>
+           <p> ♥ We designed (Eric) and coded (Amanda) this website together ♥ </p>
         </footer>
     )
 }
