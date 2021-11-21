c606bbb681c7fd31023b631a9895745971605dd2
Amanda Guerriero <ag.codingclass@gmail.com>
2021-11-20 16:55:38 -0800

    Added viewing option

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -5,7 +5,8 @@ import './style.css'
 function Footer() {
     return (
         <footer className= "flex-row space-between px-1">
-           <p> ♥ We designed (Eric) and coded (Amanda) this website together ♥ </p>
+           <div className="made-with-love"> ♥ We designed (Eric) and coded (Amanda) this website together ♥ </div>
+           <div className="browser-support">*Please view using Google Chrome</div>
         </footer>
     )
 }
