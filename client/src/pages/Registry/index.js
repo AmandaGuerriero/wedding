3dec44a31351fc2582e9214d6cd603ab32222d14
Amanda Guerriero <ag.codingclass@gmail.com>
2021-12-10 20:52:09 -0800

    Need to figure out Tracy handling

--- client/src/pages/Registry/index.js
+++ client/src/pages/Registry/index.js
@@ -9,25 +9,17 @@ function GiftsForm() {
   class App extends Component{
     constructor(props) {
       super(props);
-      this.state = {option: "bears"};
     }
   }
-  const [formState, setFormState] = useState({ name: '', amount: '', what: '', });
+  const [formState, setFormState] = useState({ name: '', amount: '', nameTracy: '', amountTracy: '' });
   const [bears, setBears] = useState(null);
 
   const [errorMessage, setErrorMessage] = useState('');
-  const { name, amount, what} = formState;
-
-  const options = [
-    {
-      label: "Bears",
-      value: "bears"
-    }
-  ]
+  const { name, amount, nameTracy, amountTracy} = formState;
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (e.target.option === 'bears') {
+    if (e.target.name != null) {
       API.saveBears({
         name: formState.name,
         amount: formState.amount
@@ -36,6 +28,20 @@ function GiftsForm() {
         .catch(err => console.log(err));
       console.log('Submit Form', formState);
       triggerSuccess(name, amount)
+    }
+  };
+
+  const handleSubmitTracy = (e) => {
+    e.preventDefault();
+    if (e.target.nameTracy != null) {
+      API.saveBears({
+        tracyName: formState.nameTracy,
+        tracyAmount: formState.amountTracy
+      })
+        .then(response => response.json())
+        .catch(err => console.log(err));
+      console.log('Submit Form', formState);
+      // triggerSuccess(name, amount)
     }
   };
 
@@ -54,28 +60,21 @@ function GiftsForm() {
 
 
   const handleChange = (e) => {
-    // if (e.target.name === 'email') {
-    //   const isValid = validateEmail(e.target.value);
-    //   if (!isValid) {
-    //     setErrorMessage('Your email is invalid.');
-    //   } else {
-    //     setErrorMessage('');
-    //   }
-    // } else {
-    //   if (!e.target.value.length) {
-    //     setErrorMessage(`${e.target.name} is required.`);
-    //   } else {
-    //     setErrorMessage('');
-    //   }
-    // }
     if (!errorMessage) {
       setFormState({ ...formState, [e.target.name]: e.target.value });
       console.log('Handle Form', formState);
     }
   };
 
+  const handleChangeTracy = (e) => {
+    if (!errorMessage) {
+      setFormState({ ...formState, [e.target.nameTracy]: e.target.value });
+      console.log('Handle Form', formState);
+    }
+  };
+
   function triggerSuccess(name) {
-    window.alert("Thank you," + name + "!" + "We are so excited to visit the bears!")
+    window.alert("Thank you," + name + "!")
     window.location.href = "/registry"
   }
 
@@ -83,6 +82,7 @@ function GiftsForm() {
   return (
     <section>
       <div className="gifts-container">
+        {/* Bears Gift */}
         <div className="bear-container gift">
         <h1>Help us see the Bears</h1>
         <h2>in Alaska</h2>
@@ -95,11 +95,11 @@ function GiftsForm() {
             <label htmlFor="amount">Amount:</label>
             <input type="number" name="amount" defaultValue={amount} onBlur={handleChange} />
           </div>
-          <select>
+          {/* <select>
              {options.map((option) => (
               <option value={option.value} onBlur={handleChange}>{option.label}</option>
           ))}
-          </select>
+          </select> */}
           <button data-testid="button" type="submit">Submit</button>
         </form>
       {/* display Bears from the API */}
@@ -114,9 +114,25 @@ function GiftsForm() {
           ))}
         </div>
       )}
+      </div>
+      {/* Tracy Arm Gift */}
+      <div className="tracy-container gift">
+        <h1>Help us go to Tracy Arm</h1>
+        <h2>in Alaska</h2>
+        <form id="tracy-form" onSubmit={handleSubmitTracy}>
+          <div>
+            <label htmlFor="name">Name:</label>
+            <input type="text" name="nameTracy" defaultValue={name} onBlur={handleChangeTracy} />
+          </div>
+          <div>
+            <label htmlFor="name">Amount:</label>
+            <input type="number" name="amountTracy" defaultValue={amountTracy} onBlur={handleChangeTracy} />
+          </div>
+          <button data-testid="button" type="submit">Submit</button>
+        </form>
       {bears && (
         <div className="bears">
-          {/* Bear Exeperience Progress */}
+          {/* Tracy Arm Exeperience Progress */}
           {bears.map((bear, index) => (
             <div key={index}>
               <ProgressBar value={bear.totalTracy} max={360}/>
