b91726acbd4500bb7ca44c89fff60c02d3a38536
Amanda Guerriero <ag.codingclass@gmail.com>
2022-07-06 16:26:11 -0700

    updates to detail page

--- client/src/pages/Details/index.js
+++ client/src/pages/Details/index.js
@@ -33,7 +33,7 @@ const Details = () => {
                   </div>
                   <div className="details-details">
                     <div className="details-note1">Take a left into the industrial park off Laurel Drive (before you reach Camden Lane) and follow the signs for the brewery to the back corner of the park.</div>
-                    <div className="details-note2">While we will have some bar food available, we recommend eating dinner beforehand.</div>
+                    <div className="details-note2">This is a hosted event with beverages along with some light bites, but we recommend eating dinner beforehand.</div>
                   </div> 
               </div>
             </div>
@@ -61,12 +61,12 @@ const Details = () => {
                   <div className="details-shuttle">STAYING IN THE HOTEL BLOCK:</div>
                   <div className="details-shuttle-location">Agrarian Hotel<br />325 E Branch St<br />Arroyo Grande, CA 93420</div>
                   <div className="details-details">
-                    <div className="details-note1">Shuttles start running from the hotel around 2:30PM and then back to the hotel around 8:30PM. Please do not take this shuttle if you are not staying at the Agrarian.</div>
+                    <div className="details-note1">Shuttles start running from the hotel around 2:30PM. <b>Please do not take this shuttle if you are not staying at the Agrarian.</b> Shuttles back to the hotel begin around 8:30PM with last shuttles at 10PM.</div>
                   </div>
                   <div className="details-shuttle">ALL OTHER GUESTS:</div>
                   <div className="details-shuttle-location">Strother Park<br />1150 Huasna Rd<br />Arroyo Grande, CA 93420</div>
                   <div className="details-details">
-                    <div className="details-note1">Shuttles start running from the park around 2:30PM and then back to the park around 8:30PM. If you decide to go to straight to the afterparty following the reception and Uber back to your hotel/Airbnb at the end of the night, you are able to leave your car in the park overnight.</div>
+                    <div className="details-note1">Shuttles start running from the park around 3:00PM and there will be some shuttles that go back to the park at the end of the night. You are able to leave your car in the park overnight if you drive to and decide to Uber back to your hotel/AirBnB.</div>
                   </div>
               </div>
             </div>
@@ -92,7 +92,7 @@ const Details = () => {
                     <div className="details-time">10:00PM - CLOSE</div>
                   </div>
                   <div className="details-details">
-                    <div className="details-note1">Mason Bar is located directly next to the Agrarian Hotel. Following the ceremony, shuttles will be running alternately to the Agrarian and Strother Park. We will have some bar fare available for those who may have worked up an appetite again on the dance floor.</div>
+                    <div className="details-note1">Mason Bar is located directly next to the Agrarian Hotel. At the end of the reception, shuttles will be running alternately to the Agrarian and Strother Park. We will have some bar fare available for those who may have worked up an appetite again on the dance floor.</div>
                   </div>
               </div>
             </div>
@@ -118,13 +118,39 @@ const Details = () => {
                     <div className="details-time">10:00AM</div>
                   </div>
                   <div className="details-details">
-                    <div className="details-note1">Local cinnamon rolls, fruit, yogurt, juice, and coffee/tea will be served in the conference room of the Agrarian Hotel for all who would like to join.</div>
+                    <div className="details-note1">Local burritos, cinnamon rolls, fruit, yogurt, juice, and coffee/tea will be served in the conference room of the Agrarian Hotel for all who would like to join before heading out.</div>
                   </div>
               </div>
             </div>
           </div>
           <div className="details-col details-image">
             <img src='https://res.cloudinary.com/amandaeric/image/upload/f_auto/details/AGRARIAN.png' alt='Brewery' />
+          </div>
+        </div>
+      </div>
+      {/* Wine */}
+      <div className="details-section-header">
+        <img className="details-olive-branch" src="https://res.cloudinary.com/amandaeric/image/upload/f_auto/site-assets/olive-branch.png" alt="Olive Branch" />
+        <div className="details-section-title">Wine Stop</div>
+        <div className="details-row">
+          <div className="details-col details-text">
+            <div className="details-section">
+              <img className="details-bullet-icon" src="https://res.cloudinary.com/amandaeric/image/upload/f_auto/site-assets/bullet-point.png" alt="Star icon as bullet point" />
+              <div className="details-name">CLAIBORNE & CHURCHILL WINERY</div>
+              <div className="details-info">
+                <div className="details-address">2649 Carpenter Canyon Road<br />San Luis Obispo, CA 93401</div>
+                  <div className="details-date-block">
+                    <div className="details-date">SUNDAY, JULY 17TH</div>
+                    <div className="details-time">12:00PM</div>
+                  </div>
+                  <div className="details-details">
+                    <div className="details-note1">Eric & Amanda will be hanging out here for the whole day. We will have a dedicated area with some snacks and wine, of course, if anyone wants to pop by.</div>
+                  </div>
+              </div>
+            </div>
+          </div>
+          <div className="details-col details-image">
+            <img src='https://res.cloudinary.com/amandaeric/image/upload/f_auto/details/CLAIBORNE.png' alt='Winery' />
           </div>
         </div>
       </div>
